7cc638ef779e952473d7955e2f3582e8732405f7
Neenu Kurian <nkurian@salmon.com>
2019-04-23 11:14:20 +0200

    added error handling

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-
+//document.addEventListener("DOMContentLoaded", function (event) {
+    
     window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+    
+    if (!('webkitSpeechRecognition' in window)) {
+        alert("Unable to use the Speech Recognition API");
+    }
 
     const recognition = new SpeechRecognition();
+   // recognition.continuous = true;
     const icon = document.querySelector('i.fa.fa-microphone')
     let paragraph = document.createElement('p');
     let container = document.querySelector('.text-box');
     container.appendChild(paragraph);
-    const sound = document.querySelector('.sound');
-
+   // const sound = document.querySelector('.sound');
+    
     icon.addEventListener('click', () => {
-        sound.play();
+        // sound.play();
         dictate();
     });
 
@@ -19,21 +24,35 @@ document.addEventListener("DOMContentLoaded", function (event) {
         recognition.onresult = (event) => {
             if (typeof (event.results) === 'undefined') { //Something is wrong…
                 recognition.stop();
+                console.log('something went wrong, please try again');
                 return;
             }
-            console.log('event results',event.results);
+            //console.log('event results',event.results);
             if (event.results[0].isFinal) {
                 const speechToText = event.results[0][0].transcript;
 
                 paragraph.textContent = speechToText;
+                console.log('isfinal');
             }
         }
+
+        recognition.onnomatch = (event) => {
+            let result = document.getElementById('search-result');
+            result.innerText = 'no matching results found';
+            console.log('no matching results found');
+            recognition.stop();
+            return;
+         };
+
+        recognition.onerror =  (event) => {
+            let result = document.getElementById('search-result');
+            result.innerText = 'something went wrong, please try again';
+            console.log(event.error);
+            recognition.stop();
+            return;
+        };
+
+        console.log('event results', event.results);
+        
     }
 
-    const synth = window.speechSynthesis;
-    const speak = (action) => {
-        utterThis = new SpeechSynthesisUtterance(action());
-        synth.speak(utterThis);
-    };
-
-});
